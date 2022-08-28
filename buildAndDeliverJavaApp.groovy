d7174a5909434fb4486da9ed2aa3b608a7bb3f18
Oussemabhouri <oussema.bhouri@jsmd-group.com>
2022-08-28 21:08:19 +0200

    initial commit

---
+++ buildAndDeliverJavaApp.groovy
@@ -0,0 +1,55 @@
+import groovy.transform.Field
+
+@Field
+Map defaults = [
+    run_tests: true,
+]
+
+def call(Map config) {
+    pipeline {
+        agent {
+            docker {
+                image 'maven:3-alpine'
+            }
+        }
+        stages {
+            stage('AUTOMATED PIPELINE') {
+                when {
+                    branch 'master'
+                }
+                stages {
+                    stage('Build with tests') {
+                        when {
+                            equals expected: true, actual: config.run_tests
+                        }
+                        steps {
+                            sh 'mvn -B clean package'
+                        }
+                    }
+                    stage('Build without tests') {
+                        when {
+                            equals expected: false, actual: config.run_tests
+                        }
+                        steps {
+                            sh 'mvn -B -DskipTests clean package'
+                        }
+                    }
+                    stage('Test') {
+                        steps {
+                            sh 'mvn test'
+                        }
+                        post {
+                            always {
+                                junit 'target/surefire-reports/*.xml'
+                            }
+                        }
+                    }
+                    stage('Deliver') {
+                        steps {
+                            sh 'chmod +x ./scripts/deliver.sh && ./scripts/deliver.sh'
+                        }
+                    }
+                }
+            }
+        }
+}
